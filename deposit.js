dc735f63a0bf16157ce73af7c1622907df8eb2a6
deboloper-rahat194940 <git config --global user.email relaxingnasheed194940@gmail.com>
2024-01-28 14:02:58 +0600

    js completed

--- deposit.js
+++ deposit.js
@@ -1,18 +1,25 @@
-document.getElementById('btn-user').addEventListener("click" , function (){
+document.getElementById('btn-user').addEventListener("click" , function () {
    var maguraw = document.getElementById('magu') ;
    var magustring = maguraw.value ;
    var freshmagu = parseFloat(magustring) ;
-   var suraw = document.getElementById('pu') ;
-   var pustring = puraw.innerText ;
-   var freshpu = parseFloat(pustring) ;
-   
-   var equationdeposit = freshmagu + freshsu ;
-   suraw.innerText = equationdeposit ;
+   var depositmulraw = document.getElementById('deposit-mul') ;
+   var depositmulstring = depositmulraw.innerText ;
+   var freshdepositmul = parseFloat(depositmulstring) ;
 
-   var puraw = document.getElementById('pu');
-   var pustring = puraw.innerText ;
-   var freshpu = parseFloat(pustring) ;
+   var balancemulraw = document.getElementById('balance-mul') ;
+  var balancemulstring = balancemulraw.innerText ;
+  var freshbalancemul = parseFloat(balancemulstring)
 
-   var equationdepositbalance = freshmagu + freshpu ;
-   puraw.innerText =  equationdepositbalance ;
+  if (maguraw.value === ""){
+   alert("টেহা রজমা দিলে জমা দে তোর টেয়া। মজা করচ ক্যা? সতানে কি লাড়া দ্যায়? হুদাও বাটন এডা টিবস। টেহার পরিমান দে কত টেয়া জমা দিবি এয়ার পর টেহা জমা দে । ভালা হইযা ভালা হইতে টেয়া লাগে না। ")
+   var halum = document.getElementById('btn-user').addEventListener("click", setAttribute(this.ariaDisabled)) 
+}
+
+   var equationDeposit = freshmagu + freshdepositmul ;
+  depositmulraw.innerText = equationDeposit ;
+
+  var depositbalaceequation = freshmagu + freshbalancemul ;
+  balancemulraw.innerText = depositbalaceequation ;
+
+  maguraw.value = '' ;
 })
--- withdraw.js
+++ withdraw.js
@@ -1,20 +1,29 @@
-document.getElementById('btn-user-2').addEventListener("click" , function (){
+document.getElementById('btn-user-2').addEventListener("click" , function () {
     var haguraw = document.getElementById('hagu') ;
     var hagustring = haguraw.value ;
     var freshhagu = parseFloat(hagustring) ;
-    var turaw = document.getElementById('tu') ;
-    var tustring = turaw.innerText ;
-    var freshtu = parseFloat(tustring) ;
-    
-    var equationwithdraw = freshhagu - freshtu ;
-    turaw.innerText = equationwithdraw ;
- 
-    var puraw = document.getElementById('pu');
-    var pustring = puraw.innerText ;
-    var freshpu = parseFloat(pustring) ;
- 
-    var equationwithdrawbalance = freshhagu - freshtu ;
-    turaw.innerText =  equationwithdrawbalance ;
+    var withdrawmulraw = document.getElementById('withdraw-mul') ;
+    var withdrawmulstring = withdrawmulraw.innerText ;
+    var freshwithdrawmul = parseFloat(withdrawmulstring)
 
-    hagustring = "" ;
- })
+    var balancemulraw = document.getElementById('balance-mul') ;
+    var balancemulstring = balancemulraw.innerText ;
+    var freshbalancemul = parseFloat(balancemulstring)
+
+    if (freshhagu > freshbalancemul){
+        alert('হারামজাদা তোর একাউন্টে এত টেহা বর্তমানে আ্যাভেইলএবল নাই। ভাগ অশিব্ব। বাপের হোটেল পাইচচ? আগে নিজের  একাউন্টের ব্যালেন্স দেখ। এহ কিতা ভাবচচ, আমরা তোগোর মতো হোলাইনের লাই সব ব্যাবস্থা করি রাহি। বাটন এডাত টিপতি হাইত্তি ন। এরুম অশব্বি মার্কা কাম কইল্লে। ভালা হইযা ভালা হইতে টেয়া লাগে না।')
+        var ganja = document.getElementById('btn-user-2').addEventListener("click", setAttribute(this.ariaDisabled)) 
+    }
+    if (haguraw.value === ""){
+        alert("টেহা লইলে উডা তোর টেয়া। মজা করচ ক্যা? সতানে কি লাড়া দ্যায়? হুদাও বাটন এডা টিবস। টেহার পরিমান দে এয়ার পর টেহা উডা অবশ্যই আগে তোর একাউন্টের ব্যালেন্স ভালা করি দেহি ল। ভালা হইযা ভালা হইতে টেয়া লাগে না। ")
+        var halum = document.getElementById('btn-user-2').addEventListener("click", setAttribute(this.ariaDisabled)) 
+    }
+    var withdrawequation = freshwithdrawmul + freshhagu ;
+    withdrawmulraw.innerText = withdrawequation ;
+
+    var balancewithdrawequation = freshbalancemul - freshhagu ;
+    balancemulraw.innerText = balancewithdrawequation ;
+
+    haguraw.value = "" ;
+  
+})
